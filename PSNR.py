abd29f02e06a38ee5cab5700cba88a8e39a10204
Tatsuya <pruthvikanth007@gmail.com>
2024-03-11 22:53:14 +0530

    fix(PSNR.py): :bug: fixed PSNR calculation to account for colored images

--- PSNR.py
+++ PSNR.py
@@ -2,7 +2,11 @@ from math import log10, sqrt
 import cv2 
 import numpy as np 
   
-def calcPSNR(original, compressed): 
+def calcPSNR(original, compressed):
+    # Convert images to float32 for calculation
+    original = original.astype(np.float32)
+    compressed = compressed.astype(np.float32)
+    
     mse = np.mean((original - compressed) ** 2) 
     if(mse == 0):  
         return 100
